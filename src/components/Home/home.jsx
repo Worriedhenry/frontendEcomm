6243b6c411c1ce0297bf74b276380a83d2206470
Ankitsharma2222 <ankitsharma220603@gmail.com>
2023-02-27 15:31:17 +0530

    maked nav bar

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,8 +1,12 @@
 import { Box , styled } from '@mui/material';
+import Mycarousel from './carousel';
 import Nav from "./navbar"
 function home(){
     return(
+        <>
         <Nav />
+        <Mycarousel />
+        </>
     )
 }
 export default home;
