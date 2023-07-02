c22a80ef5b860a9b8d75aa83c24147d3c8213e35
Ankitsharma2222 <ankitsharma220603@gmail.com>
2023-07-02 19:15:27 +0530

    minor fixes and added products

--- src/data_resourses/productdatas/Elctronics.js
+++ src/data_resourses/productdatas/Elctronics.js
@@ -2,9 +2,10 @@ const ElectronicData = [
     
     { 
         key: 'product1',
+        _id:"64a1045e4c81d287f2384049",
         url: 'https://rukminim1.flixcart.com/image/200/200/xif0q/cases-covers/back-cover/j/i/6/mycas-black-vv-y35-qcase-original-imagj87uwxzarrzk.jpeg?q=70', 
         title: {
-            shortTitle: 'Plain cases and covers',
+            shortTitle: 'Realme C33 back cover',
             company:""
 
         }, 
@@ -12,46 +13,49 @@ const ElectronicData = [
             mrp: 1195,
             cost: 625,
             discount: '47%',
-            offer:"Just ₹99"
+            offer:"Just ₹191"
         }
     },
     { 
         key: 'product2',
-        url: 'https://rukminim1.flixcart.com/image/200/200/kx50gi80/projector/r/9/p/zeb-pixa-play-12-5-6-dobly-audio-led-projector-zebronics-original-imag9z3yujqmzqt4.jpeg?q=70', 
+        _id:"64a16e496436bd72c425806b",
+        url: 'https://rukminim1.flixcart.com/image/416/416/xif0q/projector/d/7/j/-original-imaghcmghfkbtw4x.jpeg?q=70', 
         title: {
             shortTitle: 'Projectors',
-            company:"zebronics"
+            company:"Zebronics"
    
         },
         price: {
-            mrp: 1499,
-            cost: 899,
-            discount: '40%',
-            offer:"From ₹6999"
+            mrp: 99990,
+            cost: 70990,
+            discount: '20%',
+            offer:"Upto 20% off"
         }
 
  
     },
     { 
         key: 'product3',
-        url: 'https://rukminim1.flixcart.com/image/200/200/kql8sy80/monitor/o/u/h/l27e-30-27-66bekac2in-lenovo-original-imag4kkqzmu4pkan.jpeg?q=70', 
+        _id:"64a124de4c81d287f238412b",
+        url: 'https://rukminim1.flixcart.com/image/416/416/xif0q/television/d/k/e/-original-imagm2e6ztgrawkg.jpeg?q=70', 
         title: {
-            shortTitle: 'Moniters',
-            company:"Samsung "
+            shortTitle: 'Smart TV',
+            company:"Sony "
 
         }, 
         price: {
             mrp: 499,
             cost: 166,
             discount: '66%',
-            offer:"From ₹8279"
+            offer:"Upto 40% off"
 
         }
  
     },
     { 
         key: 'product4',
-        url: 'https://rukminim1.flixcart.com/image/200/200/knyxqq80/dslr-camera/r/y/x/digital-camera-eos-m50-mark-ii-eos-m50-mark-ii-canon-original-imag2gzkexzqhyhu.jpeg?q=70', 
+        _id:"64a169c96436bd72c425803e",
+        url: 'https://rukminim1.flixcart.com/image/416/416/juu4jgw0/dslr-camera/x/c/s/200d-ii-200d-ii-canon-original-imaffvh6yrfjq4nh.jpeg?q=70', 
         title: {
             shortTitle: 'Cameras',
             company:"Canon"
@@ -64,6 +68,7 @@ const ElectronicData = [
     },
     { 
         key: 'product5',
+        _id:"64a10dcb4c81d287f2384092",
         url: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70',
         title: {
             shortTitle: 'Best of hair dryers',
@@ -75,21 +80,22 @@ const ElectronicData = [
 
         }
     },
-    { 
-        key: 'product6',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70',
-        title: {
-            shortTitle: 'Table Fans',
-            company:"Orient & Bajaj"
+    // { 
+    //     key: 'product6',
+    //     url: 'https://rukminim1.flixcart.com/image/150/150/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70',
+    //     title: {
+    //         shortTitle: 'Table Fans',
+    //         company:"Orient & Bajaj"
 
-        }, 
-        price: {
-            offer:"From ₹1249"
+    //     }, 
+    //     price: {
+    //         offer:"From ₹1249"
 
-        }
-    },
+    //     }
+    // },
     { 
         key: 'product7',
+        _id:"64a10dcb4c81d287f2384092",
         url: 'https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70',
         title: {
             shortTitle: 'Printers',
@@ -98,7 +104,7 @@ const ElectronicData = [
         }, 
         price: {
 
-            offer:"From ₹3999"
+            offer:"Upto 20% off"
 
 
         }
--- src/data_resourses/productdatas/Toysdata.js
+++ src/data_resourses/productdatas/Toysdata.js
@@ -2,55 +2,58 @@ const Toydata = [
     
     { 
         key: 'product1',
+        _id:"64a111064c81d287f23840be",
         url: 'https://rukminim1.flixcart.com/image/200/200/kqgyhe80/stroller-pram/m/g/r/easy-to-push-baby-pram-with-quick-one-hand-folding-blue-mm-50-a-original-imag4h6bzrb6fngh.jpeg?q=70', 
         title: {
             shortTitle: 'Baby Gear',
-            company:"meme & more"
+            company:"LuvLap Galaxy"
 
         }, 
         price: {
             mrp: 1195,
             cost: 625,
             discount: '47%',
-            offer:"Upto 70% off"
+            offer:"Upto 20% off"
         }
     },
     { 
         key: 'product2',
-        url: 'https://rukminim1.flixcart.com/image/200/200/k0plpjk0/remote-control-toy/9/g/k/4-function-remote-control-high-speed-big-racing-car-toy-funkey-original-imafkg33umd8dy93.jpeg?q=70', 
+        _id:"64a1757b6436bd72c42580bb",
+        url: 'https://rukminim1.flixcart.com/image/832/832/krce64w0/shoe/j/p/5/6-hmi82-adidas-cblack-stone-blunit-original-imag55y2b8pvagkx.jpeg?q=70', 
         title: {
-            shortTitle: 'Remote control toys',
+            shortTitle: 'Shoes',
             company:"Buy Now"
    
         },
         price: {
-            mrp: 1499,
-            cost: 899,
-            discount: '40%',
-            offer:"Upto 80% off"
+            mrp: 2499,
+            cost: 1999,
+            discount: '57%',
+            offer:"Upto 57% off"
         }
 
  
     },
-    { 
-        key: 'product3',
-        url: 'https://rukminim1.flixcart.com/image/200/200/kql8sy80/lipstick/7/m/y/21-sara-s-favorite-sensational-liquid-matte-pack-of-3-touch-of-original-imag4kgnbmynydnw.jpeg?q=70', 
-        title: {
-            shortTitle: 'Makeup Essentials',
-            company:"Lakme swiss beauty & more "
+    // { 
+    //     key: 'product3',
+    //     url: 'https://rukminim1.flixcart.com/image/200/200/kql8sy80/lipstick/7/m/y/21-sara-s-favorite-sensational-liquid-matte-pack-of-3-touch-of-original-imag4kgnbmynydnw.jpeg?q=70', 
+    //     title: {
+    //         shortTitle: 'Makeup Essentials',
+    //         company:"Lakme swiss beauty & more "
 
-        }, 
-        price: {
-            mrp: 499,
-            cost: 166,
-            discount: '66%',
-            offer:"Upto 60% off"
+    //     }, 
+    //     price: {
+    //         mrp: 499,
+    //         cost: 166,
+    //         discount: '66%',
+    //         offer:"Upto 60% off"
 
-        }
+    //     }
  
-    },
+    // },
     { 
         key: 'product4',
+        _id:"64a178eb6436bd72c42580f0",
         url: 'https://rukminim1.flixcart.com/image/200/200/klzhq4w0/jam-spread/w/q/u/510-chocolate-peanut-butter-crunchy-510g-jar-nut-butter-original-imagyzpqqfaguxny.jpeg?q=70', 
         title: {
             shortTitle: 'Food Spreads',
@@ -64,9 +67,10 @@ const Toydata = [
     },
     { 
         key: 'product5',
+        _id:"64a11a5d4c81d287f23840d1",
         url: 'https://rukminim1.flixcart.com/image/200/200/kzzw5u80/coffee/s/b/x/-original-imagbwf3wvhzfh5z.jpeg?q=70',
         title: {
-            shortTitle: 'Coffee Powder',
+            shortTitle: 'Nestle Coffee Powder',
             company:"Nescafe, continentals & more"
 
         }, 
@@ -77,10 +81,11 @@ const Toydata = [
     },
     { 
         key: 'product6',
-        url: 'https://rukminim1.flixcart.com/image/200/200/kjuby4w0/deodorant/v/c/d/400-one8-intense-fresh-deo-pack-of-2-200ml-x-2-2-perfume-body-original-imafzbn4nv2zhccp.jpeg?q=70',
+        _id:"64a121594c81d287f23840ff",
+        url: 'https://rukminim1.flixcart.com/image/416/416/xif0q/deodorant/o/h/w/-original-imaga36aktvbsxmx.jpeg?q=70',
         title: {
             shortTitle: 'Deodorents & Perfumes',
-            company:"Denver , fogg & more"
+            company:"SET WET"
 
         }, 
         price: {
@@ -90,7 +95,8 @@ const Toydata = [
     },
     { 
         key: 'product7',
-        url: 'https://rukminim1.flixcart.com/image/200/200/jxz0brk0/stuffed-toy/n/t/s/4-feet-pink-very-beautiful-best-quality-for-special-gift-125-13-original-imafgv92puzkdytg.jpeg?q=70',
+        _id:"64a11dba4c81d287f23840e8",
+        url: 'https://rukminim1.flixcart.com/image/416/416/kqse07k0/stuffed-toy/e/o/j/elephant-soft-toy-with-dress-grey-20-fusked-original-imag4pzpbdhrw5xt.jpeg?q=70',
         title: {
             shortTitle: 'Soft toys',
             company:"Stuffed toys, Plush toys & more"
